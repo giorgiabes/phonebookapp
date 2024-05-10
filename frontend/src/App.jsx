5a1c861a72d2c0ba80ca829c9ae65e6ade299b5b
giorgiabes <abesadze.george@gmail.com>
2024-05-10 16:30:26 +0400

    The Phonebook Step 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Alice Smith", number: "434-220-5050" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const addPerson = (e) => {
     e.preventDefault();
@@ -26,6 +30,13 @@ const App = () => {
     });
   };
 
+  const search = (e) => {
+    e.preventDefault();
+    // Uncomment if you need filtered results
+    // setPersons(filteredPersons);
+    setSearchTerm("");
+  };
+
   const handleNameChange = (e) => {
     setNewName(e.target.value);
   };
@@ -34,9 +45,21 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  const handleCharChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={search}>
+        filter: <input value={searchTerm} onChange={handleCharChange} />
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -50,7 +73,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {filteredPersons.map((person) => (
           <li key={person.name}>
             {person.name} {person.number}
           </li>
