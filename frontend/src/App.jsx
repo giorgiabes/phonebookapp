051c6b0dd654dca9c6e0952de47f400280be26cd
giorgiabes <abesadze.george@gmail.com>
2024-05-10 15:35:23 +0400

    The Phonebook Step 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,38 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addPerson = (e) => {
+    e.preventDefault();
+
+    const newObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newObject));
+    setNewName("");
+  };
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
