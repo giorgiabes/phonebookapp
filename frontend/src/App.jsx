945c69d2054e1a81f29ce479b1eb31ec533727ce
giorgiabes <abesadze.george@gmail.com>
2024-05-11 13:28:20 +0400

    The Phonebook Step 6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addPerson = (e) => {
     e.preventDefault();
@@ -22,15 +24,17 @@ const App = () => {
       number: newNumber,
     };
 
-    persons.map((person) => {
-      if (person.name === newName) {
-        alert(`${newName} is already added to phonebook`);
-      } else {
-        setPersons(persons.concat(newObject));
-        setNewName("");
-        setNewNumber("");
-      }
-    });
+    const personExists = persons.find((p) => p.name === newName);
+
+    if (personExists) {
+      alert(`${newName} is already added to phonebook`);
+      setNewName("");
+      setNewNumber("");
+    } else {
+      setPersons(persons.concat(newObject));
+      setNewName("");
+      setNewNumber("");
+    }
   };
 
   const search = (e) => {
