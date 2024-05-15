de0d0736c2761a44dfdf46fd5ee6640bacf47308
giorgiabes <abesadze.george@gmail.com>
2024-05-15 19:55:00 +0400

    Phonebook backend step 3

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,17 @@ app.get("/api/persons", (requests, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (requests, response) => {
+  const id = Number(requests.params.id);
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
