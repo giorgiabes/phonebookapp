ca86a40ce18c692d9b2e5bd4d89bff33fc4ed83f
giorgiabes <abesadze.george@gmail.com>
2024-05-22 22:23:03 +0400

    uibuild

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -64,7 +64,6 @@ const App = () => {
       }
     } else {
       personService.create(newObject).then((response) => {
-        setPersons(persons.concat(response.data));
         setNewName("");
         setNewNumber("");
         setNotification({
