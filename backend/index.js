688cd62393f7098ec51544e3095d26d98b9b0b5e
giorgiabes <abesadze.george@gmail.com>
2024-05-24 14:18:32 +0400

    Phonebook database, step 3

--- backend/index.js
+++ backend/index.js
@@ -54,10 +54,11 @@ app.get("/api/persons/:id", (request, response) => {
 
 // delete one person
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => error);
 });
 
 // add one person
